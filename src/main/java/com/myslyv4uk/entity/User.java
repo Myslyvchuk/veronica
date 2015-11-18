fe1dc3b4356a04e5fd60c54157857896cda7917e
Bogdan Myslyvchuk <bmys@softserveinc.com>
2015-11-18 21:15:49 +0200

    added persistence layer to spring configuration

---
+++ src/main/java/com/myslyv4uk/entity/User.java
@@ -0,0 +1,14 @@
+package com.myslyv4uk.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
