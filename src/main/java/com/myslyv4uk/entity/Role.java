76f9ab21e244b40721d666fec6cdaa5ddccead3e
Bogdan Myslyvchuk <bmys@softserveinc.com>
2015-11-20 14:22:44 +0200

    added entity Role and changed columns names

---
+++ src/main/java/com/myslyv4uk/entity/Role.java
@@ -0,0 +1,41 @@
+package com.myslyv4uk.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "role_id", unique = true, nullable = false)
+    private Integer roleId;
+
+    @Column(name = "role", length = 10)
+    private String role;
+
+    public Role() {
+        super();
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
--- src/main/java/com/myslyv4uk/entity/User.java
+++ src/main/java/com/myslyv4uk/entity/User.java
@@ -1,8 +1,13 @@
 package com.myslyv4uk.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -10,15 +15,29 @@ import javax.persistence.Table;
 public class User {
 
     @Id
-    @GeneratedValue
-    private Integer id;
+    @SequenceGenerator(name = "users_user_id_seq", sequenceName = "users_user_id_seq", allocationSize = 101)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_user_id_seq")
+    @Column(name = "user_id", unique = true, nullable = false)
+    private Integer userId;
 
-    public Integer getId() {
-        return id;
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public Integer getUserId() {
+        return userId;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
 }
